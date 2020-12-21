8d0a589ef2bea4e215f2a487262f17b741739c43
Alexey <miloserdoff.liox@yandex.ru>
2020-12-21 14:27:30 +0400

    Почти все готово

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let openButton = document.querySelector(".profile__edit-button");
+let overlay = document.querySelector(".overlay");
+let closeButton = overlay.querySelector(".overlay__button");
+function toggleForm() {
+  overlay.classList.toggle("overlay_active");
+}
+openButton.addEventListener("click", toggleForm);
+closeButton.addEventListener("click", toggleForm);
+overlay.addEventListener("click", (event) => {
+  if (event.target === event.currentTarget) {
+    toggleForm();
+  }
+});
+
+let form = overlay.querySelector(".form");
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  let nameInput = overlay.querySelector(".form__item_name");
+  let jobInput = overlay.querySelector(".form__item_job");
+  let nameValue = document.querySelector(".profile__info_name");
+  let jobValue = document.querySelector(".profile__info_about");
+  nameValue.textContent = nameInput.value;
+  jobValue.textContent = jobInput.value;
+}
+form.addEventListener("submit", handleFormSubmit);
+let submitButton = overlay.querySelector(".form__button");
+submitButton.addEventListener("click", () => {
+  overlay.classList.remove("overlay_active");
+});
